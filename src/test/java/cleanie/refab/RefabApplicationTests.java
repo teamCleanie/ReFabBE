2350c2053e493df794817c17a9e6efafecc4f379
stoneHee99 <kiit0901@gmail.com>
2024-10-22 16:39:35 +0900

    chore: 프로젝트 초기 생성

---
+++ src/test/java/cleanie/refab/RefabApplicationTests.java
@@ -0,0 +1,13 @@
+package cleanie.refab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

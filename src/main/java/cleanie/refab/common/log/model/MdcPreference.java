9b919e976da0bb609ffc21a26394e4776484c7a3
stoneHee99 <kiit0901@gmail.com>
2024-10-22 18:18:43 +0900

    config: 요청과 응답 로깅을 위한 Mapped Diagnostic Context 필터 생성

--- src/main/java/cleanie/refab/common/log/model/MdcPreference.java
+++ src/main/java/cleanie/refab/common/log/model/MdcPreference.java
@@ -1,2 +1,9 @@
-package cleanie.refab.common.log.model;public enum MdcPreference {
+package cleanie.refab.common.log.model;
+
+public enum MdcPreference {
+    REQUEST_ID,
+    REQUEST_METHOD,
+    REQUEST_URI,
+    REQUEST_TIME,
+    REQUEST_IP
 }

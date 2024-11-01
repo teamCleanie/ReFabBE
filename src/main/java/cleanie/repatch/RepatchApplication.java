3bdeffa766a521b2ffb8d07019991a726c9f24d7
c0smosaur <sjkang539@gmail.com>
2024-11-01 14:46:28 +0900

    refactor: S3Config 수정
    - @Value 애너테이션 제거
    - AwsS3Properties 클래스로 속성값 주입하도록 변경
    - main application에 @EnableConfigurationProperties 애너테이션 추가
    - application.yml 속성값 이름 변경(변수명이 static 키워드와 중복되므로 static-region으로 변경)

--- src/main/java/cleanie/repatch/RepatchApplication.java
+++ src/main/java/cleanie/repatch/RepatchApplication.java
@@ -1,9 +1,12 @@
 package cleanie.repatch;
 
+import cleanie.repatch.common.s3.config.AwsS3Properties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AwsS3Properties.class)
 public class RepatchApplication {
 
     public static void main(String[] args) {
--- src/main/java/cleanie/repatch/common/s3/config/S3Config.java
+++ src/main/java/cleanie/repatch/common/s3/config/S3Config.java
@@ -5,34 +5,30 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableConfigurationProperties(AwsS3Properties.class)
 public class S3Config {
 
-    private final String AWS_REGION;
-    private final String AWS_ACCESS_KEY;
-    private final String AWS_SECRET_KEY;
+    private final AwsS3Properties awsS3Properties;
 
-    public S3Config(
-            @Value("${cloud.aws.region.static}") String AWS_REGION,
-            @Value("${cloud.aws.credentials.access-key}") String AWS_ACCESS_KEY,
-            @Value("${cloud.aws.credentials.secret-key}") String AWS_SECRET_KEY) {
-        this.AWS_REGION = AWS_REGION;
-        this.AWS_ACCESS_KEY = AWS_ACCESS_KEY;
-        this.AWS_SECRET_KEY = AWS_SECRET_KEY;
+    public S3Config(AwsS3Properties awsS3Properties){
+        this.awsS3Properties = awsS3Properties;
     }
 
     @Bean
     public AmazonS3 amazonS3Client() {
-        AWSCredentials credentials = new BasicAWSCredentials(AWS_ACCESS_KEY, AWS_SECRET_KEY);
+        AWSCredentials credentials = new BasicAWSCredentials(
+                awsS3Properties.getAccessKey(),
+                awsS3Properties.getSecretKey());
 
         return AmazonS3ClientBuilder
                 .standard()
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
-                .withRegion(AWS_REGION)
+                .withRegion(awsS3Properties.getRegion())
                 .build();
     }
 }

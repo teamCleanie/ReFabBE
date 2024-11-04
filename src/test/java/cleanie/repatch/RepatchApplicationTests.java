a4ad126a20db0f1209b0f57ee05eadb9d469ed3e
stoneHee99 <kiit0901@gmail.com>
2024-11-04 17:00:26 +0900

    fix: gradle sonar 명령어 순서 변경

--- src/test/java/cleanie/repatch/RepatchApplicationTests.java
+++ src/test/java/cleanie/repatch/RepatchApplicationTests.java
@@ -1,11 +1,8 @@
 package cleanie.repatch;
 
 import cleanie.repatch.setting.annotation.IntegrationTest;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
 class RepatchApplicationTests {
 
     @IntegrationTest

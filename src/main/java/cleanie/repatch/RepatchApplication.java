4b6f8199cc69ffb7050fc4aa749597a50412505d
c0smosaur <sjkang539@gmail.com>
2024-11-08 01:23:14 +0900

    refactor: jpa auditing 활성

--- src/main/java/cleanie/repatch/RepatchApplication.java
+++ src/main/java/cleanie/repatch/RepatchApplication.java
@@ -4,9 +4,11 @@ import cleanie.repatch.common.s3.config.AwsS3Properties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableConfigurationProperties(AwsS3Properties.class)
+@EnableJpaAuditing
 public class RepatchApplication {
 
     public static void main(String[] args) {

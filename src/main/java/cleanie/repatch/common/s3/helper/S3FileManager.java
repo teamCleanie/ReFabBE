d52fb7413fe88661120b4e59832f52da8c5790f3
c0smosaur <sjkang539@gmail.com>
2024-11-09 00:47:36 +0900

    refactor: s3 업로드 확장자 포함하도록 수정

--- src/main/java/cleanie/repatch/common/s3/helper/S3FileManager.java
+++ src/main/java/cleanie/repatch/common/s3/helper/S3FileManager.java
@@ -2,7 +2,6 @@ package cleanie.repatch.common.s3.helper;
 
 import cleanie.repatch.common.s3.config.AwsS3Properties;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
@@ -22,9 +21,11 @@ public class S3FileManager {
     }
 
     public String uploadFile(MultipartFile file, String fileName) throws IOException {
-        amazonS3Client.putObject(new PutObjectRequest(BUCKET_NAME, fileName, file.getInputStream(), null)
-                .withCannedAcl(CannedAccessControlList.PublicRead));
-        return getFileUrl(fileName);
+        String originalFileName = file.getOriginalFilename();
+        String extension = originalFileName.substring(originalFileName.lastIndexOf('.'));
+        String newFileName = fileName + extension;
+        amazonS3Client.putObject(new PutObjectRequest(BUCKET_NAME, newFileName, file.getInputStream(), null));
+        return getFileUrl(newFileName);
     }
 
     public String getFileUrl(String fileName){

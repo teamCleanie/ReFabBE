9f121b1667f89f23cec4b6afdd2cb43e9ee1b9aa
c0smosaur <sjkang539@gmail.com>
2024-11-13 21:03:10 +0900

    refactor: 파일 확장자 검증 로직 수정

--- src/main/java/cleanie/repatch/photo/component/PhotoValidator.java
+++ src/main/java/cleanie/repatch/photo/component/PhotoValidator.java
@@ -1,26 +1,28 @@
 package cleanie.repatch.photo.component;
 
+import cleanie.repatch.photo.domain.PhotoExtension;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.EnumSet;
 
 @Component
 public class PhotoValidator {
 
-    private final Set<String> allowedExtensions;
-
-    public PhotoValidator() {
-        this.allowedExtensions = new HashSet<>(Set.of(".jpg", ".jpeg", ".png", ".webp"));
-    }
+    private final EnumSet<PhotoExtension> ALLOWED_EXTENSIONS = EnumSet.allOf(PhotoExtension.class);
 
     public boolean validateFileAsImage(MultipartFile file) {
         String originalFileName = file.getOriginalFilename();
         if (originalFileName == null || !originalFileName.contains(".")) {
             return false;
         }
-        String extension = originalFileName.substring(originalFileName.lastIndexOf('.'));
-        return allowedExtensions.contains(extension);
+        return hasValidExtension(originalFileName);
+    }
+
+    public boolean hasValidExtension(String fileName) {
+        String extension = fileName.substring(fileName.lastIndexOf('.'));
+
+        return ALLOWED_EXTENSIONS.stream()
+                .anyMatch(ex -> extension.endsWith(ex.getExtension()));
     }
 }

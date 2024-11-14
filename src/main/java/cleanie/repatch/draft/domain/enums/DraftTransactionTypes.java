49228ec5e3f6c3507a546f9ec65063de765d972f
c0smosaur <sjkang539@gmail.com>
2024-11-15 00:14:10 +0900

    refactor: 임시저장 기능 분리
    - 임시저장글에 저장될 거래방식 @ElementCollection enum 분리
    - 일급 컬렉션으로 래핑

---
+++ src/main/java/cleanie/repatch/draft/domain/enums/DraftTransactionTypes.java
@@ -0,0 +1,48 @@
+package cleanie.repatch.draft.domain.enums;
+
+import cleanie.repatch.post.domain.enums.TransactionType;
+import cleanie.repatch.post.domain.enums.TransactionTypes;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Getter
+@Embeddable
+@RequiredArgsConstructor
+public class DraftTransactionTypes {
+
+    @ElementCollection(targetClass = DraftTransactionType.class)
+    @CollectionTable(name = "draft_transaction_type", joinColumns = @JoinColumn(name = "draft_id"))
+    @Enumerated(EnumType.STRING)
+    private final Set<DraftTransactionType> draftTransactionTypes;
+
+    public DraftTransactionTypes() {
+        this.draftTransactionTypes = new HashSet<>();
+    }
+
+    public DraftTransactionTypes updateDraftTransactionTypes(
+            DraftTransactionTypes draftTransactionTypes, Set<DraftTransactionType> updates) {
+        draftTransactionTypes.draftTransactionTypes.clear();
+        draftTransactionTypes.draftTransactionTypes.addAll(updates);
+
+        return draftTransactionTypes;
+    }
+
+    public Set<String> createStringSet(Set<DraftTransactionType> transactions) {
+        return transactions.stream()
+                .map(Enum::name)
+                .collect(Collectors.toSet());
+    }
+
+    public TransactionTypes toTransactionTypes() {
+        Set<TransactionType> transactionTypeSet = this.draftTransactionTypes.stream()
+                .map(draftTransactionType -> TransactionType.valueOf(draftTransactionType.name()))
+                .collect(Collectors.toSet());
+
+        return new TransactionTypes(transactionTypeSet);
+    }
+}

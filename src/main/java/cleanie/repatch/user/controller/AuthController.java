1469b58c279cc23e3445b1dc61d600a5884c4194
Seokhee Park <seokhee_park@nexcubecorp.com>
2024-11-14 11:07:53 +0900

    setting: REST docs 생성 관련 설정 추가

--- src/main/java/cleanie/repatch/user/controller/AuthController.java
+++ src/main/java/cleanie/repatch/user/controller/AuthController.java
@@ -1,8 +1,5 @@
 package cleanie.repatch.user.controller;
 
-import cleanie.repatch.common.security.annotation.Auth;
-import cleanie.repatch.common.security.annotation.MemberOnly;
-import cleanie.repatch.common.security.domain.Accessor;
 import cleanie.repatch.user.model.request.OAuthLoginRequest;
 import cleanie.repatch.user.model.response.AuthResponse;
 import cleanie.repatch.user.service.AuthService;
@@ -12,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("api/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
@@ -25,11 +22,5 @@ public class AuthController {
                 .header("Authorization", "Bearer " + response.getAccessToken())
                 .header("Refresh-Token", response.getRefreshToken())
                 .body(response);
-    }
-
-    @GetMapping("/test")
-    @MemberOnly
-    public String test(@Auth Accessor accessor) {
-        return "test";
     }
 }

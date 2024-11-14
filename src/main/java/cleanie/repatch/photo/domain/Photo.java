284b8383174c2563dc083d94028c777fdd9efa71
c0smosaur <sjkang539@gmail.com>
2024-11-14 23:18:49 +0900

    refactor: 도메인 클래스로 생성, 검증, DTO 로직 이동

--- src/main/java/cleanie/repatch/photo/domain/Photo.java
+++ src/main/java/cleanie/repatch/photo/domain/Photo.java
@@ -1,9 +1,15 @@
 package cleanie.repatch.photo.domain;
 
 import cleanie.repatch.common.domain.BaseEntity;
+import cleanie.repatch.common.exception.FileTypeMismatchException;
+import cleanie.repatch.common.exception.model.ExceptionCode;
+import cleanie.repatch.photo.domain.enums.PhotoExtension;
+import cleanie.repatch.photo.model.response.PhotoResponse;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
+
+import java.util.Arrays;
 
 @Entity
 @Getter
@@ -16,4 +22,34 @@ public class Photo extends BaseEntity {
     private Long id;
     private String imageUrl;
     private Long postId;
+    private Long draftId;
+
+    public static Photo toPhoto(String imageUrl) {
+        if (validatePhotoUrl(imageUrl)) {
+            return new Photo(null, imageUrl, null, null);
+        }
+        throw new FileTypeMismatchException(ExceptionCode.FILE_TYPE_NOT_SUPPORTED);
+    }
+
+    public static boolean validatePhotoUrl(String imageUrl) {
+        String extension = imageUrl.substring(imageUrl.lastIndexOf('.')).toLowerCase();
+        return Arrays.stream(PhotoExtension.values())
+                .anyMatch(ex -> extension.endsWith(ex.getExtension()));
+    }
+
+    public void addPostIdToPhoto(Photo photo, Long postId) {
+        photo.postId = postId;
+    }
+
+    public void addDraftIdToPhoto(Photo photo, Long draftId) {
+        photo.draftId = draftId;
+    }
+
+    public void removeDraftIdFromPhoto(Photo photo) {
+        photo.draftId = null;
+    }
+
+    public PhotoResponse toPhotoResponse(Photo photo) {
+        return new PhotoResponse(photo.getId(), photo.getImageUrl());
+    }
 }
--- src/main/java/cleanie/repatch/post/domain/Post.java
+++ src/main/java/cleanie/repatch/post/domain/Post.java
@@ -1,15 +1,21 @@
 package cleanie.repatch.post.domain;
 
 import cleanie.repatch.common.domain.BaseEntity;
-import cleanie.repatch.photo.domain.Photo;
+import cleanie.repatch.common.exception.BadRequestException;
+import cleanie.repatch.common.exception.model.ExceptionCode;
+import cleanie.repatch.draft.domain.Draft;
+import cleanie.repatch.photo.domain.Photos;
 import cleanie.repatch.post.domain.enums.FabricType;
 import cleanie.repatch.post.domain.enums.PostType;
 import cleanie.repatch.post.domain.enums.TransactionTypes;
+import cleanie.repatch.post.model.request.PostRequest;
+import cleanie.repatch.post.model.response.PostIdResponse;
+import cleanie.repatch.post.model.response.PostResponse;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
-import java.util.List;
+import java.util.stream.Stream;
 
 @Entity
 @Getter
@@ -31,12 +37,81 @@ public class Post extends BaseEntity {
     private String unit;
     private String price;
     private String content;
-    private Boolean isPublished;
 
     @Embedded
     private TransactionTypes transactionTypes;
 
-    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    @JoinColumn(name = "postId")
-    private List<Photo> photos;
+    @Embedded
+    private Photos photos;
+
+    public static Post toPost(PostRequest request) {
+
+        return Post.builder()
+                .postType(request.postType())
+                .fabricType(request.fabricType())
+                .title(request.title())
+                .unit(request.unit())
+                .price(request.price())
+                .content(request.content())
+                .transactionTypes(new TransactionTypes(request.transactionTypes()))
+                .photos(new Photos())
+                .build();
+    }
+
+    public Post updatePost(Post post, PostRequest request, Photos photos) {
+        if (!validatePost(request)) {
+            throw new BadRequestException(ExceptionCode.INVALID_POST_REQUEST);
+        }
+        TransactionTypes transactions = post.getTransactionTypes();
+        post.fabricType = request.fabricType();
+        post.title = request.title();
+        post.unit = request.unit();
+        post.price = request.price();
+        post.content = request.content();
+        post.transactionTypes = transactions.updateTransactionTypes(transactions, request.transactionTypes());
+        post.photos = photos;
+
+        return post;
+    }
+
+    public static Post publishDraft(Draft draft) {
+        return Post.builder()
+                .postType(draft.getPostType())
+                .fabricType(draft.getFabricType())
+                .title(draft.getTitle())
+                .unit(draft.getUnit())
+                .price(draft.getPrice())
+                .content(draft.getContent())
+                .transactionTypes(draft.getDraftTransactionTypes().toTransactionTypes())
+                .photos(new Photos())
+                .build();
+    }
+
+    public boolean validatePost(PostRequest request) {
+        return Stream.of(
+                request.postType().getPostEngName(),
+                request.fabricType().getFabricEngName(),
+                request.title(),
+                request.unit(),
+                request.price(),
+                request.content()
+        ).noneMatch(String::isBlank) && !request.photoIds().isEmpty()
+                && !request.transactionTypes().isEmpty();
+    }
+
+    public PostResponse toPostResponse(Post post) {
+        TransactionTypes transactions = post.getTransactionTypes();
+
+        return new PostResponse(
+                post.getId(), post.getPostType(),
+                post.getTitle(), post.getFabricType(),
+                post.getUnit(), post.getPrice(),
+                post.getContent(), transactions.createStringSet(transactions.getTransactionTypes()),
+                Photos.toPhotoResponses(post.getPhotos()),
+                post.getCreatedAt(), post.getModifiedAt());
+    }
+
+    public PostIdResponse toPostIdResponse(Post post) {
+        return new PostIdResponse(post.getId());
+    }
 }
